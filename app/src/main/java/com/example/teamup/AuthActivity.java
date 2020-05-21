63d3d59eeebe038a042848cb6a347b31268e54dd
RiffLord <bruno.pezer@hotmail.com>
2020-05-21 13:41:26 +0200

    FirebaseAuth & UI

--- app/src/main/java/com/example/teamup/AuthActivity.java
+++ app/src/main/java/com/example/teamup/AuthActivity.java
@@ -1,20 +1,31 @@
 package com.example.teamup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class AuthActivity extends AppCompatActivity {
+    private static final String TAG = "AuthActivity";
 
     private FirebaseAuth mAuth;
 
@@ -28,26 +39,30 @@ public class AuthActivity extends AppCompatActivity {
         //  Riferimenti agli elementi dell'UI
         ImageView image = findViewById(R.id.imageView_teamUp);
         TextView teamUpDescription = findViewById(R.id.textView_desc);
-        final Button loginButton = findViewById(R.id.button_login);
-        final Button registerButton = findViewById(R.id.button_register);
+        Button mLoginButton = findViewById(R.id.button_login);
+        Button mRegisterButton = findViewById(R.id.button_register);
         Button discoverButton = findViewById(R.id.button_discover);
 
         //  Registrazione di listener per i pulsanti
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                registerButton.setEnabled(false);
-
                 login();
             }
         });
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginButton.setEnabled(false);
-
                 register();
+            }
+        });
+
+        discoverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //  TODO: Intent per inziare l'Activity relativa a IUI-7
+                Toast.makeText(AuthActivity.this, "Porta l'utente a IUI-7", Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -57,22 +72,24 @@ public class AuthActivity extends AppCompatActivity {
         super.onStart();
         //  TODO: verificare se l'utente è già autenticato e in tal caso chiamare teamUp()
         FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) teamUp();
     }
 
     private void login() {
         final Dialog loginDialog = new Dialog(AuthActivity.this);
         loginDialog.setContentView(R.layout.login_dialog);
 
-        Button loginButton = loginDialog.findViewById(R.id.button_login);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        Button dialogLoginButton = loginDialog.findViewById(R.id.button_login);
+        dialogLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //  Verifica che le EditText per l'indirizzo e-mail e la password non siano vuote
                 EditText etEmail = loginDialog.findViewById(R.id.editText_email);
+                EditText etPass = loginDialog.findViewById(R.id.editText_pass);
                 String sEmail = etEmail.getText().toString();
+                String sPass = etPass.getText().toString();
 
-
-                if (!sEmail.equals("")) teamUp();
+                if (!sEmail.equals("") && !sPass.equals("")) signIn(sEmail, sPass);
             }
         });
 
@@ -83,15 +100,22 @@ public class AuthActivity extends AppCompatActivity {
         final Dialog registerDialog = new Dialog(AuthActivity.this);
         registerDialog.setContentView(R.layout.register_dialog);
 
-        Button bRegister = registerDialog.findViewById(R.id.button_register);
-        bRegister.setOnClickListener(new View.OnClickListener() {
+        Button dialogRegisterButton = registerDialog.findViewById(R.id.button_register);
+        dialogRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //  Verifica che l'EditText per l'indirizzo e-mail non sia vuota
                 EditText etEmail = registerDialog.findViewById(R.id.editText_email);
-                String sEmail = etEmail.getText().toString();
+                EditText etPass = registerDialog.findViewById(R.id.editText_pass);
+                EditText etName = registerDialog.findViewById(R.id.editText_name);
+                EditText etSurname = registerDialog.findViewById(R.id.editText_surname);
 
-                if (!sEmail.equals("")) teamUp();
+                String sEmail = etEmail.getText().toString();
+                String sPass = etPass.getText().toString();
+                String displayName = etName.getText().toString() + ' ' + etSurname.getText().toString();
+
+                if (!sEmail.equals(""))
+                    createAccount(displayName, sEmail, sPass);
             }
         });
 
@@ -100,7 +124,102 @@ public class AuthActivity extends AppCompatActivity {
 
     //  TODO: modificare il nome di questa funzione
     private void teamUp() {
-        Intent teamUpIntent = new Intent(this, MainActivity.class);
-        startActivity(teamUpIntent);
+        if (mAuth.getCurrentUser() != null) {
+            if (mAuth.getCurrentUser().isEmailVerified()) {
+                Intent teamUpIntent = new Intent(this, MainActivity.class);
+                startActivity(teamUpIntent);
+            } else verifyUser();
+        }
+    }
+
+    //========================= Firebase Authentication =========================//
+
+    private void createAccount(final String displayName, final String email, final String pass) {
+        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    //  TODO: sostituire questo con un metodo apposito che crea un messaggio Toast per l'utente
+                    Log.e(TAG, "Create FirebaseUser unsuccessful\n");
+                } else {
+                    //  Aggiorna il profilo dell'utente impostando la stringa displayName come nome utente
+                    final FirebaseUser user = mAuth.getCurrentUser();
+                    if (user != null) {
+                        user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder().setDisplayName(displayName).build();
+                                    user.updateProfile(profileUpdate);
+
+                                    signIn(email, pass);
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
+    }
+
+    private void signIn(String email, String pass) {
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    //  TODO: creare un Toast con un messaggio di errore significativo
+                    Log.e(TAG, "Login Failed\n");
+                } else teamUp();
+            }
+        });
+    }
+
+    private void verifyUser() {
+        AlertDialog.Builder verifyDialogBuilder = new AlertDialog.Builder(this);
+        //  TODO: trasformare la stringa in una risorsa in strings.xml
+        verifyDialogBuilder.setTitle("Verify Account");
+        verifyDialogBuilder.setMessage("Please check your e-mail for the verification link to activate your account.");
+        verifyDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (mAuth.getCurrentUser() != null) {
+                    mAuth.getCurrentUser().reload().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                if (mAuth.getCurrentUser().isEmailVerified()) teamUp();
+                                else {
+                                    //  If the user isn't verified, opens a chooser for the
+                                    //  device's e-mail clients to allow the user to complete
+                                    //  the process.
+                                    Toast.makeText(AuthActivity.this,
+                                            "Please verify your account before proceeding",
+                                            Toast.LENGTH_SHORT).show();
+
+                                    Intent emailClientIntent = new Intent(Intent.ACTION_MAIN);
+                                    emailClientIntent.addCategory(Intent.CATEGORY_APP_EMAIL);
+                                    try {
+                                        startActivity(emailClientIntent);
+                                    } catch (ActivityNotFoundException e) {
+                                        //  TODO: creare un Toast con un messaggio di errore significativo
+                                    }
+                                }
+                            } //else //TODO: creare un Toast con un messaggio di errore significativo
+                        }
+                    });
+                }
+            }
+        });
+
+        verifyDialogBuilder.setNeutralButton("Resend Link", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (mAuth.getCurrentUser() != null)
+                    mAuth.getCurrentUser().sendEmailVerification();
+            }
+        });
+
+        AlertDialog verifyDialog = verifyDialogBuilder.create();
+        verifyDialog.show();
     }
 }
--- app/src/main/java/com/example/teamup/MainActivity.java
+++ app/src/main/java/com/example/teamup/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.teamup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
@@ -20,11 +27,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = "MainActivity";
 
     private AppBarConfiguration mAppBarConfiguration;
 
-    //  Dichiarato public per poter essere visto e usato da activity_profile_drawer.xml
-    public void logout() {
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+
+    //  UI
+    ImageView mProfilePic;
+    TextView mDisplayName;
+    TextView mEmail;
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
         Intent logoutIntent = new Intent(this, AuthActivity.class);
         startActivity(logoutIntent);
     }
@@ -45,6 +61,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        //  Ottiene i riferimenti agli elementi dell'UI
+        View headerView = navigationView.getHeaderView(0);
+        mDisplayName = (TextView) headerView.findViewById(R.id.display_name);
+        mEmail = (TextView) headerView.findViewById(R.id.email);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -59,6 +80,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //  quando l'utente clicca sull'opzione appropriata.
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            mUser = mAuth.getCurrentUser();
+            //  Aggiorna l'UI con i dati dell'utente
+            mDisplayName.setText(mUser.getDisplayName());
+            mEmail.setText(mUser.getEmail());
+        }
+
+        Button discoverButton = findViewById(R.id.button_discover);
+        discoverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //  TODO: Intent per inziare l'Activity relativa a IUI-7
+                Toast.makeText(MainActivity.this, "Porta l'utente a IUI-7", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
@@ -77,13 +115,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.nav_logout:
-                Intent logoutIntent = new Intent(this, AuthActivity.class);
-                startActivity(logoutIntent);
-                //  TODO: FirebaseAuth logout
-                break;  //  Forse non serve?
-        }
+        if (menuItem.getItemId() == R.id.nav_logout) logout();
         return true;
     }
 }
