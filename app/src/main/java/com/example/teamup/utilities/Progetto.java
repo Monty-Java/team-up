87a55a318128cfa893be5618d6c83c78fd91476c
pucaccio <lorenzovalentino94@libero.it>
2020-05-26 21:55:38 +0200

    26 Mag 2020 - Lorenzo: Ho creato la classe Progetto, Utente, Leader e Teammate. Inoltre ho creato una classe Enum, per le etichette dei progetti. Le classi Utente, Leader e Teammate ovviamente non sono complete, ma servivano per l'implementazione di Progetto. Ho infine risolto un paio di TODO, relativi all'inserimento di stringhe in string.xml

---
+++ app/src/main/java/com/example/teamup/utilities/Progetto.java
@@ -0,0 +1,94 @@
+package com.example.teamup.utilities;
+import java.util.*;
+import java.lang.reflect.*;
+
+class Progetto {
+    private String id;
+    private String titolo;
+    private String descrizione;
+    private List<Tag> etichette;
+    private Leader leader;
+    private List<Teammate> personale;
+    private List<String> obiettiviRaggiunti;
+    private List<String> obiettiviDaRaggiungere;
+
+    Progetto(Leader creator) {
+        this.leader = creator;
+        //genera id
+        UUID uuid = UUID.randomUUID();
+        id = uuid.toString();
+    }
+    void setTitolo(Utente user,String title) {
+        if(this.leader == user) {
+            titolo = title;
+        }
+        else {
+            System.out.println("Error, you're not the leader of this project!");
+        }
+    }
+    void setDescrizione(Utente user,String description) {
+        if(this.leader == user) {
+            descrizione = description;
+        }
+        else {
+            System.out.println("Error, you're not the leader of this project!");
+        }
+    }
+    void setEtichette(Utente user,List<Tag> labels) {
+        if(this.leader == user) {
+            etichette = labels;
+        }
+        else {
+            System.out.println("Error, you're not the leader of this project!");
+        }
+    }
+    void setPersonale(Utente user, List<Teammate> mates) {
+        if(this.leader == user) {
+            personale = mates;
+        }
+        else {
+            System.out.println("Error, you're not the leader of this project!");
+        }
+    }
+    void setObiettiviRaggiunti(Utente user, List<String> reachedTarget) {
+        if(this.leader == user) {
+            obiettiviRaggiunti = reachedTarget;
+        }
+        else {
+            System.out.println("Error, you're not the leader of this project!");
+        }
+    }
+    void setObiettiviDaRaggiungere(Utente user, List<String> targetToReach) {
+        if(this.leader == user) {
+            obiettiviDaRaggiungere = targetToReach;
+        }
+        else {
+            System.out.println("Error, you're not the leader of this project!");
+        }
+    }
+
+    String getId() {
+        return id;
+    }
+    String getTitolo() {
+        return titolo;
+    }
+    String getDescrizione() {
+        return descrizione;
+    }
+    List<Tag> getEtichette() {
+        return etichette;
+    }
+    Leader getLeader() {
+        return leader;
+    }
+    List<Teammate> getPersonale() {
+        return personale;
+    }
+    List<String> getObiettiviRaggiunti() {
+        return obiettiviRaggiunti;
+    }
+    List<String> getObiettiviDaRaggiungere() {
+        return obiettiviDaRaggiungere;
+    }
+}
